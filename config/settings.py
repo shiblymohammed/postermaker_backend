5ea304a5b428c5519f770212ea2b8e507fae972e
shibilymohammed <shiblymohammed75@gmail.com>
2025-11-14 15:54:50 +0530

    Fix DATABASE_URL parsing for Render deployment

--- config/settings.py
+++ config/settings.py
@@ -88,8 +88,8 @@ DATABASE_URL = config('DATABASE_URL', default=None)
 
 if DATABASE_URL:
     DATABASES = {
-        'default': dj_database_url.config(
-            default=DATABASE_URL,
+        'default': dj_database_url.parse(
+            DATABASE_URL,
             conn_max_age=600,
             conn_health_checks=True,
         )
